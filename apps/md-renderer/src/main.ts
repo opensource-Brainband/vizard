d85d793186b5fce6c0d9c7ff55f3573c0a805845
whabl0220 <stargalaxy1579@gmail.com>
2025-08-16 17:24:35 +0900

    docs: "add simple comments in main.ts"

--- apps/md-renderer/src/main.ts
+++ apps/md-renderer/src/main.ts
@@ -1,7 +1,9 @@
 import MarkdownIt from 'markdown-it';
 import markdownText from '../docs/test.md?raw';
 
+// Initialize MarkdownIt
 const md = new MarkdownIt();
+// Get the preview element and render the markdown content
 const preview = document.getElementById('preview');
 if (preview) {
   preview.innerHTML = md.render(markdownText);
