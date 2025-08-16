84683782ccf2a45b9c75fb9680c1d1cf12c9b185
keyboardbear <daniel010203@naver.com>
2025-08-16 22:43:51 +0900

    feat: add initial renderLineChart.ts file for line chart rendering

---
+++ packages/src/render/renderLineChart.ts
@@ -0,0 +1,11 @@
+import * as d3 from 'd3'
+
+type Datum = Record<string, string | number>
+
+interface LineChartData {
+	
+}
+
+export function renderLineChart(container: HTMLElement, chartData: LineChartData) {
+	
+}
