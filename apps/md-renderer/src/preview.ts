a4bc116a1772454d0a007b4b1818772322064994
whabl0220 <stargalaxy1579@gmail.com>
2025-08-16 17:20:20 +0900

    remove: "delete preview.ts file"

--- apps/md-renderer/src/preview.ts
+++
@@ -1,8 +0,0 @@
-import MarkdownIt from 'markdown-it';
-import markdownText from '../docs/test.md?raw';
-
-const md = new MarkdownIt();
-const preview = document.getElementById('preview');
-if (preview) {
-  preview.innerHTML = md.render(markdownText);
-}
