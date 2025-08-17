2250149cd521d5a9c57ccb80d77fb21783b6d9ba
whabl0220 <stargalaxy1579@gmail.com>
2025-08-17 22:03:11 +0900

    feat: add logo to webpage

--- apps/web/src/views/welcomeHtml.ts
+++ apps/web/src/views/welcomeHtml.ts
@@ -1,3 +1,5 @@
+import logo from '../assets/logo.svg?raw';
+
 export const welcomeHtml = `
   <div style="width: 100vw; min-height: 100vh; background: white; overflow: hidden; flex-direction: column; justify-content: flex-start; align-items: center; display: flex">
     <div style="align-self: stretch; height: 80px; padding: 20px; background: #21203C; overflow: hidden; justify-content: center; align-items: center; display: flex">
@@ -5,8 +7,9 @@ export const welcomeHtml = `
     </div>
     <div style="flex: 1 1 0; padding-top: 10px; padding-bottom: 10px; overflow: hidden; flex-direction: column; justify-content: center; align-items: center; display: flex">
       <div style="padding-left: 90px; padding-right: 90px; overflow: hidden; flex-direction: column; justify-content: center; align-items: center; gap: 60px; display: flex">
-  <div class="vizard-fadein" style="text-align: center; color: black; font-size: 50px; font-family: Pretendard Variable, Pretendard, sans-serif; font-weight: 500; word-wrap: break-word;">Welcome to Vizard</div>
-  <div id="get-started-btn" class="vizard-fadein" style="cursor:pointer; padding-left: 40px; padding-right: 40px; padding-top: 15px; padding-bottom: 15px; background: #3D3D3D; overflow: hidden; border-radius: 27px; flex-direction: column; justify-content: center; align-items: center; gap: 10px; display: flex;">
+      <div class="logo-container vizard-fadein" style="display: flex; justify-content: center; align-items: center; width: 200px; height: 200px;">${logo}</div>
+        <div class="vizard-fadein" style="text-align: center; color: black; font-size: 50px; font-family: Pretendard Variable, Pretendard, sans-serif; font-weight: 500; word-wrap: break-word;">Welcome to Vizard</div>
+        <div id="get-started-btn" class="vizard-fadein" style="cursor:pointer; padding-left: 40px; padding-right: 40px; padding-top: 15px; padding-bottom: 15px; background: #3D3D3D; overflow: hidden; border-radius: 27px; flex-direction: column; justify-content: center; align-items: center; gap: 10px; display: flex;">
           <div style="text-align: center; color: white; font-size: 20px; font-family: Pretendard Variable, Pretendard, sans-serif; font-weight: 500; word-wrap: break-word">Get Started</div>
         </div>
       </div>
